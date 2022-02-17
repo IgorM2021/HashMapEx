27ff9687c43638dec8c386b30b067cab06635a33
PC <igormaric84@hotmail.com>
2022-02-17 09:26:37 +0100

    Initial commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import java.util.Scanner;
+
+     public class Fibonacci {
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Write the order of fibonacci you want:");
+        int number = Integer.parseInt(scan.nextLine());
+        ArrayList<Long> fibonacci = new ArrayList<Long>();
+
+        fibonacci.add(1L);
+        fibonacci.add(1L);
+        for (int i = 2; i < number;i++) {
+            fibonacci.add( fibonacci.get(i - 1) + fibonacci.get(i - 2));
+        }
+
+        System.out.print("This corresponds to: ");
+        System.out.println(fibonacci.get(number - 1));
+
+
+    }
+}
