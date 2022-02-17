93a0b1f2b412dc6ec4e425b75e54809e7df314fe
PC <igormaric84@hotmail.com>
2022-02-17 21:23:14 +0100

    izmene koje su napravljene

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -20,7 +20,6 @@ import java.util.Scanner;
 
         System.out.print("This corresponds to: ");
         System.out.println(fibonacci.get(number - 1));
-ngdnondo
-
-    }
+ghjklövu
+        hgj    }
 }
